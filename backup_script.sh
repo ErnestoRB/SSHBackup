2a7844858a57ab394419b2dbb01d41293c9ca82e
Ernesto <ernestorb_@outlook.com>
2022-05-27 19:01:33 -0500

    Corregir crontab y corregir error de script nunca termina

--- backup_script.sh
+++ backup_script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 log() {
 	PREFIX="[LOG]"
 	echo "${PREFIX}: $(date +"%d/%m/%Y %T") -> $*"
@@ -15,22 +16,16 @@ if [ ! -e $config ]; then
     echo "folder=/var/www" >>$config
     log "Archivo de configuracion creado"
 fi
-
+    
 user=$( cat $config | awk -F"=" '/=/ && $1 == "user" { print $2 }' )
 host=$( cat $config | awk -F"=" '/=/ && $1 == "host" { print $2 }' )
 folder=$( cat $config | awk -F"=" '/=/ && $1 == "folder" { print $2 }' )
-
-tar -cvzf /tmp/respaldo.tar.gz $folder
-i=1
+tar -czf /tmp/respaldo.tar.gz $folder
 fecha=$(date +"%d-%m-%y_%H-%M")
-false
-while [ $? -ne 0 -o $i -eq 10 ]; do
-    let i++
-    scp /tmp/respaldo.tar.gz ${user:-root}@${host:-localhost}:~/"respaldo${fecha}.tar.gz"
-done
-if [ $i -eq 10 ]; then
-    logError "No se pudo hacer el respaldo a ${user}@${host}... ¿Probablemente esté caído?"
-else
+if scp /tmp/respaldo.tar.gz -o ConnectTimeout=1 -o ConnectionAttempts=5 ${user:-root}@${host:-localhost}:~/"respaldo${fecha}.tar.gz"
+then
     log "Respaldo hecho ${user}@${host}: $fecha"
     rm /tmp/respaldo.tar.gz
+else
+    logError "No se pudo hacer el respaldo a ${user}@${host}... ¿Probablemente esté caído?"
 fi
